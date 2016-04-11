12a395c34e3186339895bab5ef95866caaa8d55c
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-04-11 22:53:17 +0700

    tambah fungsi alert

--- src/main/java/com/rizki/mufrizal/aplikasi/musik/controller/MusikController.java
+++ src/main/java/com/rizki/mufrizal/aplikasi/musik/controller/MusikController.java
@@ -4,12 +4,18 @@ import com.rizki.mufrizal.aplikasi.musik.App;
 import com.rizki.mufrizal.aplikasi.musik.model.Musik;
 import java.io.File;
 import java.util.List;
+import java.util.Optional;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -55,6 +61,12 @@ public class MusikController {
     private Label repeatMusik;
 
     @FXML
+    private MenuItem about;
+
+    @FXML
+    private MenuItem quit;
+
+    @FXML
     @SuppressWarnings("FieldMayBeFinal")
     private TableView<Musik> tabelMusik = new TableView<>();
 
@@ -90,6 +102,29 @@ public class MusikController {
         repeat.setDisable(Boolean.TRUE);
 
         repeatMusik.setText(String.valueOf(Boolean.FALSE));
+
+        about.setOnAction((ActionEvent event) -> {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("About");
+            alert.setHeaderText("About Our Project");
+            alert.setContentText("Project ini dibuat dalam rangka belajar javafx \n Developer : Rizki Mufrizal <mufrizalrizki@gmail.com>");
+
+            alert.showAndWait();
+        });
+
+        quit.setOnAction((ActionEvent event) -> {
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Konfirmasi");
+            alert.setHeaderText("Apakah anda ingin keluar ?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                Platform.exit();
+                System.exit(0);
+            } else {
+                alert.close();
+            }
+        });
     }
 
     @FXML
@@ -187,7 +222,7 @@ public class MusikController {
         }
     }
 
-    public void jalankanMusik(int selectedIndex) {
+    private void jalankanMusik(int selectedIndex) {
         File file = new File(tabelMusik.getItems().get(selectedIndex).getPathLagu());
         String path = file.toURI().toASCIIString();
 
